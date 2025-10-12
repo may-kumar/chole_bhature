105bf5cadee5406688391ea73ac6fb7f8605040a
Shail <shvaidya@ucsd.edu>
2025-10-11 17:56:54 -0700

    updated q4

--- project_files/project1/fir128_q4/fir.cpp
+++ project_files/project1/fir128_q4/fir.cpp
@@ -28,6 +28,7 @@ void fir (
     acc = 0;
     Shift_Accum_Loop:
     for (i = N - 1; i > 0; i--) {
+        //#pragma HLS pipeline off
         shift_reg[i] = shift_reg[i - 1];
         acc += shift_reg[i] * c[i];
     }
