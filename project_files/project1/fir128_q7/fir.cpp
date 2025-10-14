dc5dc6809a97d91dbc2b62d17d8701fe1327ad72
Mayank Kumar <mayank.kumar1123@gmail.com>
2025-10-13 17:47:23 -0700

    Add commented combined optimzaztions for 17

--- project_files/project1/fir128_q7/fir.cpp
+++ project_files/project1/fir128_q7/fir.cpp
@@ -43,4 +43,41 @@ void fir (
 
 }
 
+/*
+// This is combined optimzations
+void fir (
+  data_t *y,
+  data_t x
+  )
+{
+    
+    coef_t c[N] = {10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+    
+    // Write your code here
+    static data_t shift_reg[N];
+
+    acc_t acc;
+    int i;
+    #pragma HLS array_partition variable = shift_reg complete
+    TDL:
+    for (i = N - 1; i >= 1; i--){
+        #pragma HLS unroll skip_exit_check
+        shift_reg[i] = shift_reg[i - 1];
+    }
+    shift_reg[0] = x;
+
+    acc = 0;
+    MAC:
+	for (i = N - 1; i >= 0 ; i--){
+        #pragma HLS unroll skip_exit_check factor=64
+        #pragma HLS pipeline
+        acc += shift_reg[i] * c[i];
+    }
+
+    *y = acc;
+
+}
+
+*/
+
 
