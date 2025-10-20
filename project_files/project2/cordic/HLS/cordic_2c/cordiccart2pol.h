675eaaba8e65bba9d34cbf71271303a6939ebda6
Mayank Kumar <mayank.kumar1123@gmail.com>
2025-10-20 16:11:25 -0700

    cordic_2b and 2c infrastructure updates

--- project_files/project2/cordic/HLS/cordic_2c/cordiccart2pol.h
+++ project_files/project2/cordic/HLS/cordic_2c/cordiccart2pol.h
@@ -5,13 +5,13 @@
 
 #define NO_ITER 12
 
-#define TOTAL_BITS_VAR 32
+#define TOTAL_BITS_VAR   16
 #define TOTAL_BITS_TABLE 8
 
 typedef ap_fixed<TOTAL_BITS_VAR, 3> data_var_t;
 typedef data_var_t data_t;
 
-typedef float data_table_t;
+typedef ap_fixed<TOTAL_BITS_TABLE, 3> data_table_t;
 
 void cordiccart2pol(data_var_t x, data_var_t y, data_var_t * r,  data_var_t * theta);
 
