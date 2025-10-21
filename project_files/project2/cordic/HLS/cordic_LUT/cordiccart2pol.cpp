2a7b500e3395b99e506d4e62b91f1b1de977691b
Shail <shvaidya@ucsd.edu>
2025-10-21 11:52:03 -0700

    Removed init function for LUT

--- project_files/project2/cordic/HLS/cordic_LUT/cordiccart2pol.cpp
+++ project_files/project2/cordic/HLS/cordic_LUT/cordiccart2pol.cpp
@@ -5,39 +5,8 @@
 #include "cordic_LUTs.hpp"
 
 
-
-void init_cart2pol_LUTs(data_t my_LUT_th[LUT_SIZE], data_t my_LUT_r[LUT_SIZE])
-{
-	// Fill the LUT values with their appropriate R and theta values
-	for(int i=0; i<LUT_SIZE; i++){
-		ap_uint<2*W> index = i;
-		ap_fixed<W, I, AP_RND, AP_WRAP, 1> fixed_x;
-		ap_fixed<W, I, AP_RND, AP_WRAP, 1> fixed_y;
-
-		for(int j = 0; j < W; j++)
-			{
-				fixed_x[W-1-j] = index[2*W-1-j];
-				fixed_y[W-1-j] = index[W-1-j];
-			}
-
-		float _x = fixed_x;
-		float _y = fixed_y;
-
-		if((_x == 0) & (_y == 0)){
-			my_LUT_th[index] = 0;
-			my_LUT_r[index]  = 0;
-		}
-		else{
-			my_LUT_th[index] = atan2f(_y, _x);
-			my_LUT_r[index]  = sqrtf((_y*_y)+(_x*_x));
-		}
-	}
-}
-
 void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta)
 {
-	//#pragma HLS bind_storage variable=my_LUT_th type=RAM_1P    
-	//#pragma HLS bind_storage variable=my_LUT_r type=RAM_1P
 	// Convert the inputs to internal fixed point representation
 	ap_fixed<W, I, AP_RND, AP_WRAP, 1> fixed_x = x;
 	ap_fixed<W, I, AP_RND, AP_WRAP, 1> fixed_y = y;
