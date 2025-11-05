20d2aa4b61580ef8486f7cefe9759ad197fd6b66
Mayank Kumar <mayank.kumar1123@gmail.com>
2025-11-04 20:32:00 -0800

    Clean up the dft.cpp of 6b

--- project_files/project3/Q6b/dft.cpp
+++ project_files/project3/Q6b/dft.cpp
@@ -8,23 +8,15 @@ void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DT
     int i, j;
     DTYPE w;
     DTYPE c, s;
-    // Temporary arrays to hold the intermediate frequency domain results
-    DTYPE temp_real[SIZE];
-    DTYPE temp_imag[SIZE];
+
     // Calculate each frequency domain sample_iteratively
     for (i = 0; i < SIZE; i += 1) {
-        temp_real[i] = 0;
-        temp_imag[i] = 0;
-        // (2 * pi * i)/SIZE
-        // w = (2.0 * 3.141592653589 / SIZE) * (DTYPE)i;
+        real_op[i] = 0;
+        imag_op[i] = 0;
         // Calculate the jth frequency sample sequentially
         for (j = 0; j < SIZE; j += 1) {
-            // Utilize HLS tool to calculate sine and cosine values
-            // c = cos(j * w);
-            // s = -sin(j * w);
             c = cos_coefficients_table[(j * i) & (SIZE - 1)];
             s = sin_coefficients_table[(j * i) & (SIZE - 1)];
-            // Multiply the current phasor with the appropriate input sample and keep
             // running sum
             real_op[i] += (real_sample[j] * c - imag_sample[j] * s);
             imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
